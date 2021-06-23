38c1697d7b2e60b2eb4a8bffa36cf0c600144538
bisrat2020 <wow.bisrat11@gmail.com>
2021-06-22 20:41:08 -0400

    homework

--- W1d2_Node/HW/app.js
+++ W1d2_Node/HW/app.js
@@ -21,3 +21,23 @@ Array.prototype.odd = function() {
   }
 
 console.log([1,2,3,4,5,6,7,8].odd());
+
+/**1. Use setImmediate() when you want to execute some function asynchronously, but as soon as possible and after finishing the current block. 
+ * setTimeout()
+Use setTimeout() when you want to execute some function asynchronously, after a specified delay and after finishing the current block.
+ setTimeout is simply like calling the function after delay has finished.
+setImmediate is similar in this regard except that it doesn’t use queue of functions.
+
+2. nextTick() run before any other I/O event is fired, while with setImmediate(), the execution is queued behind any I/O event that is already in the queue.
+
+3. 1.Class: Buffer
+  2.console
+  3.process
+  4.global
+  5.setImmediate
+  6.clearImmediate
+  7.setInterval
+  8.clearInterval
+  9.setTimeout
+  10. clearTimeout
+*/
--- W1d2_Node/script.js
+++ W1d2_Node/script.js
@@ -12,3 +12,23 @@ setTimeout(() => {
 //nextTick always run 1st 
 process.nextTick(() => console.log("nexttick"));//1
 // the order of the queas are important 
+
+
+setImmediate(function A() {
+	console.log("1st immediate");
+});
+
+setImmediate(function B() {
+	console.log("2nd immediate");
+});
+
+process.nextTick(function C() {
+	console.log("1st process");
+});
+
+process.nextTick(function D() {
+	console.log("2nd process");
+});
+
+// First event queue ends here
+console.log("program started");
